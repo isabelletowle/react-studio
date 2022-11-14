d47ea248d6d007adb0a5cc6675d49922728de3c2
Isabelle Towle <isabelle_towle@brown.edu>
2022-11-14 11:18:03 -0500

    done

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,16 @@
 // TODO: create a component that displays a single bakery item
+import { useState } from 'react';
+
+export default function BakeryItem(props) {
+    const [count, setCount] = useState(0); 
+    const item = props.itm; 
+    const index = props.idx; 
+
+    return (
+        <div key={index}>
+            <img src={item.image} alt=""></ img>
+            <button onClick={() => props.addToCart(item)}>Add to cart</button>
+            <h2>{item.name} {item.price}</h2>
+        </div>
+    )
+}
